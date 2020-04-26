d7f415bc791e5d9ca8c92a2e0e0a82e79d4b67aa
Ben K <ben.knight.brazil@hotmail.com>
2020-04-25 19:00:27 -0600

    added contributors and licenses

--- Develop/README.md
+++ Develop/README.md
@@ -1,13 +1,19 @@
-## bennay
-## Installation
-OS and Linux install:
-   
-```install - yeah baby```
-   
-Windows install: 
-   
-```install - not so much```
-# weather app
+# Weather app
 this is a weather app
 ## Preview
 ![Picture of finished project](download.jpg)
+## Installation
+OS and Linux install:
+   
+```install - os```
+   
+Windows install: 
+   
+```install - windows```
+## bennay
+## Contributors:
+   
+Your mom's chest hair
+## Licenses
+   
+[MIT](https://choosealicense.com/licenses/mit/)
--- Develop/index.js
+++ Develop/index.js
@@ -37,7 +37,7 @@ const questions = [
         message: "What command to install on Windows?",
     },
 
-// Github user name
+    // Github user name
     {
         type: "input",
         name: "gitHubUserName",
@@ -48,7 +48,22 @@ const questions = [
         type: "input",
         name: "usageExample",
         message: "What is a good example of using this project?",
-    }
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who are the contributors to this project?",
+    },
+    {
+        type: "input",
+        name: "licenceName",
+        message: "What is the name of the licence you used?",
+    },
+    {
+        type: "input",
+        name: "licenceSite",
+        message: "What is the https link to the license site?",
+    },
 ];
 const titles = [
     "# ",
@@ -64,31 +79,29 @@ inquirer.prompt(questions).then(answers => {
     let justAnswers = Object.values(answers)
     let justTitles = Object.values(titles)
     // variable for long installation input
-    const installationSetup = titles[1] + "Installation" + "\n" + "OS and Linux install:"  + '\n' + '   '+ '\n' + "```" + answers.OsAndLinuxInstallation + "```" + '\n' + '   '+ '\n' + "Windows install: " + '\n' + '   '+ '\n' + "```" + answers.windowsInstallation + "```" + '\n';
+    const installationSetup = titles[1] + "Installation" + "\n" + "OS and Linux install:" + '\n' + '   ' + '\n' + "```" + answers.OsAndLinuxInstallation + "```" + '\n' + '   ' + '\n' + "Windows install: " + '\n' + '   ' + '\n' + "```" + answers.windowsInstallation + "```" + '\n';
 
-// Project title and description
-    fs.appendFile("README.md", titles[0] + answers.projectTitle + '\n' + answers.description + '\n', function (err) {
+    // Project title and description
+    fs.appendFileSync("README.md", titles[0] + answers.projectTitle + '\n' + answers.description + '\n', function (err) {
         if (err) throw err;
     });
-    fs.appendFile("README.md", titles[1] + "Preview" + "\n" + "![Picture of finished project](" + answers.screenshotPath + ")" + '\n', function (err) {
+    fs.appendFileSync("README.md", titles[1] + "Preview" + "\n" + "![Picture of finished project](" + answers.screenshotPath + ")" + '\n', function (err) {
         if (err) throw err;
     });
-    fs.appendFile("README.md", installationSetup, function (err) {
+    fs.appendFileSync("README.md", installationSetup, function (err) {
         if (err) throw err;
     });
-
-
-    fs.appendFile("README.md", titles[1] + answers.gitHubUserName + '\n', function (err) {
+    fs.appendFileSync("README.md", titles[1] + answers.gitHubUserName + '\n', function (err) {
         if (err) throw err;
     });
-    // fs.appendFile("README.md", answers.description + '\n', function (err) {
-    //     if (err) throw err;
-    //     console.log("saved");
-    // });
-    // fs.appendFile("README.md", answers.OsAndLinuxInstallation + '\n', function (err) {
-    //     if (err) throw err;
-    //     console.log("saved");
-    // });
+    fs.appendFileSync("README.md", titles[1] + "Contributors:" + '\n' + "   " + '\n' + answers.contributors + "\n", function (err) {
+        if (err) throw err;
+        console.log("saved");
+    });
+    fs.appendFileSync("README.md", titles[1] + "Licenses" + '\n' + "   " + '\n' + "[" + answers.licenceName + "]" + "(" + answers.licenceSite + ")", function (err) {
+        if (err) throw err;
+        console.log("saved");
+    });
     // fs.appendFile("README.md", answers.windowsInstallation + '\n', function (err) {
     //     if (err) throw err;
     //     console.log("saved");
